b04b32f658c4cd11889a44e0ef2e0b785c681f4a
GabrieleBortolai <87896150+GabrieleBortolai@users.noreply.github.com>
2025-03-31 09:08:14 +0200

    update

--- 2024_2025/Prog1/Lab/Critto/crittoFun.cpp
+++ 2024_2025/Prog1/Lab/Critto/crittoFun.cpp
@@ -18,21 +18,10 @@ int shift(char a, int s){
 }
 
 char critta(char a, int key){
-    
-    // cout<<"output"<<shift((shift(a, - find_min(a)) + (key % 25)) % 26, find_min(a))<<endl;
 
     return shift((shift(a, - find_min(a)) + (key % 26)) % 26, find_min(a));
 
 }
-
-// char critta(char a, int key){
-    
-//     // cout<<"output"<<(a + (key % 25)) % 26<<endl;
-
-//     return (a + (key % 26)) % 26;
-
-// }
-
 
 string critta_testo(string testo, int key){
     
